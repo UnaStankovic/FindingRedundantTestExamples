4fe4feda9044acf0d1eca96c511d19349caaa3c4
Una Stankovic <una_stankovic@yahoo.com>
2018-05-08 11:55:17 +0200

    Added first s(a/i)mple program for observation.

---
+++ 1_max4.c
@@ -0,0 +1,46 @@
+
+A straightforward approach is the following
+
+#include <stdio.h>
+
+int main( void )
+{   
+    int a, b, c, d;
+    int largest, smallest;
+
+    printf( "Enter four integers (separate them with spaces): " );
+    scanf( "%d %d %d %d", &a, &b, &c, &d );
+
+
+    largest = smallest = a;
+
+    if ( largest < b )
+    {
+        largest = b;
+    }
+    else if ( b < smallest )
+    {
+        smallest = b;
+    }
+    if ( largest < c )
+    {
+        largest = c;
+    }
+    else if ( c < smallest )
+    {
+        smallest = c;
+    }
+    if ( largest < d )
+    {
+        largest = d;
+    }
+    else if ( d < smallest )
+    {
+        smallest = d;
+    }
+
+    printf( "\nLargest: %d\n", largest );
+    printf( "Smallest: %d", smallest );
+
+    return 0;
+}
